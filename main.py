8423daf0c4463a2195a2d1bc60f2e1f85f0a9272
Fittergem <99085141+fittergem@users.noreply.github.com>
2026-04-21 14:57:37 +0000

    Created resistance calculations for both parallel and series circuits

--- main.py
+++ main.py
@@ -1,1 +1,53 @@
 
+def calculate_resistance(resistances: list[int], parallel: bool):
+    total_r = 0
+
+    for r in resistances:
+        if parallel:
+            total_r += 1/r
+        else:
+            total_r += r
+
+    return parallel and 1/total_r or total_r
+
+def r_inputs():
+    input_r = input("Input resistance: ")
+
+    try:
+        input_r = int(input_r)
+    except ValueError:
+        return
+    
+    return input_r
+
+def new_resistance(parallel: bool):
+    resistances = []
+
+    while True:
+        input_r = r_inputs()
+
+        if input_r == None:
+            break
+
+        resistances.append(input_r)
+
+    total_resistance = calculate_resistance(resistances, parallel)
+    print(total_resistance)
+
+def selections():
+    selection = input("1: Resistance, 2: Current, 3: Voltage... Selection: ")
+
+    if selection == "1":
+        selection = input("1: Parallel, 2: Series... Selection: ")
+
+        if selection == "1":
+            new_resistance(True)
+        else:
+            new_resistance(False)
+    elif selection == "2":
+        return
+    elif selection == "3":
+        return
+
+while True:
+    selections()
